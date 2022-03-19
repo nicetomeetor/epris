5dc1b6dda1b1798f16bea2f8d0aa200584469c14
Michael Lutsiv <nicetomeetor@gmail.com>
2022-03-19 21:06:36 +0700

    Update types

--- src/epris.types.ts
+++ src/epris.types.ts
@@ -1,3 +1,15 @@
 export type EprisConfig = {
     prefix: string;
 }
+
+export type State = {
+    [key: string]: any
+}
+
+export type Actions = {
+    [key: string]: EventListener
+}
+
+export type Getters = {
+    [key: string]: Function
+}
