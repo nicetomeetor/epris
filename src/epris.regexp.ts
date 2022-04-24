bb2c5ae1d4d055f8571ed993fb57a6fe38d24b19
Michael Lutsiv <nicetomeetor@gmail.com>
2022-04-25 01:47:27 +0700

    Add new regular expressions

--- src/epris.regexp.ts
+++ src/epris.regexp.ts
@@ -2,3 +2,5 @@ export const regExpFor = /(.*) of (.*)/;
 export const regExpFun = /(.*)\((.*)\)/;
 export const regExpArg = /(.*)(\.?)(.*)/;
 export const regExpEmpty = /\s/g;
+export const regPropModifierName = /(.*):/
+export const regPropModifierValue = /:(.*)/
