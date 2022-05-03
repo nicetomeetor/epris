eda3e293e9172141aedf65789b1a318653992435
Michael Lutsiv <nicetomeetor@gmail.com>
2022-05-03 15:44:17 +0700

    Change bind directive

--- src/directives/e-bind.ts
+++ src/directives/e-bind.ts
@@ -1,6 +1,6 @@
 import { chainElementKeys } from '../epris.parser';
 
-export default ({ rawValue, context, propModifierValue, propModifierName, node, propName  }: any) => {
+export default ({ rawValue, context, propModifierValue, node, propName  }: any) => {
     const state = context.state;
     const parsedValue = chainElementKeys(rawValue, state);
 
