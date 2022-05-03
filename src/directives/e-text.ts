42b84a046410d4de7bc4b42aadb5284ebcb7dc05
Michael Lutsiv <nicetomeetor@gmail.com>
2022-05-03 15:46:26 +0700

    Change text directive

--- src/directives/e-text.ts
+++ src/directives/e-text.ts
@@ -2,7 +2,5 @@ import { chainElementKeys } from '../epris.parser';
 
 export default ({rawValue, context, node}: any) => {
     const state = context.state;
-    const parsedValue = chainElementKeys(rawValue, state);
-
-    node.innerText = parsedValue;
+    node.innerText = chainElementKeys(rawValue, state);
 }
