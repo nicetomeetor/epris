036a8a3f2dc8fa85ddd02da1bfad3b977337694e
Michael Lutsiv <nicetomeetor@gmail.com>
2022-05-04 23:10:44 +0700

    Update state change effect

--- src/epris.reactivity.ts
+++ src/epris.reactivity.ts
@@ -1,27 +1,12 @@
-let activeEffect: Function;
+let stateChangeEffect: Function = () => {};
 
 export const watchEffect = (f: Function) => {
-    activeEffect = f;
+    stateChangeEffect = f;
     f();
-    activeEffect = null;
 };
 
-class Dependency {
-    private subscribers: Set<Function>;
+const effects = () => {
 
-    constructor() {
-        this.subscribers = new Set();
-    }
-
-    depend() {
-        if (activeEffect) {
-            this.subscribers.add(activeEffect);
-        }
-    }
-
-    notify() {
-        this.subscribers.forEach(subscriber => subscriber());
-    }
 }
 
 export const reactive = (object: { [key: string]: any }): Object => {
@@ -33,18 +18,15 @@ export const reactive = (object: { [key: string]: any }): Object => {
         object[property] = reactive(object[property]);
     }
 
-    const dep = new Dependency();
-
     return new Proxy(object, {
         get(target, property) {
-            dep.depend();
             return target[property as keyof typeof target];
         },
 
         set(target, property, value) {
             if (target[property as keyof typeof target] !== value) {
                 target[property as keyof typeof target] = reactive(value);
-                dep.notify();
+                stateChangeEffect()
             }
 
             return true;
