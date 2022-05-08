cbe4c92d187c05aa986e64b4aa4b765782c8df41
Michael Lutsiv <nicetomeetor@gmail.com>
2022-05-08 13:08:56 +0700

    Format events module

--- src/epris.events.ts
+++ src/epris.events.ts
@@ -5,45 +5,68 @@ const prefix = config.prefix + 'on:';
 
 export const isEvent = (event: string) => event.includes(prefix);
 
-export const addEvent = (el: HTMLElement, e: string, action: EventListener) => {
-    el.addEventListener(e, action);
+export const addEvent = (
+    element: HTMLElement,
+    event: string,
+    action: EventListener,
+) => {
+    element.addEventListener(event, action);
 };
 
-export const removeEvent = (el: HTMLElement, e: string, action: EventListener) => {
-    el.removeEventListener(e, action);
+export const removeEvent = (
+    element: HTMLElement,
+    event: string,
+    action: EventListener,
+) => {
+    element.removeEventListener(event, action);
 };
 
-export const removeEvents = (el: HTMLElement, events: {[key: string]: EventListener}) => {
+export const removeEvents = (
+    element: HTMLElement,
+    events: { [key: string]: EventListener },
+) => {
     Object
         .entries(events)
         .forEach(([event, handler]) => {
-            removeEvent(el, event, handler);
+            removeEvent(element, event, handler);
         });
 };
 
-export const addEvents = (el: HTMLElement, events: {[key: string]: EventListener}) => {
+export const addEvents = (
+    el: HTMLElement,
+    events: { [key: string]: EventListener },
+) => {
     Object
         .entries(events)
         .forEach(([event, handler]) => {
             addEvent(el, event, handler);
         });
-}
+};
 
-export const updateEvents = (vNode: VirtualNode, newVNode: VirtualNode)  => {
+export const updateEvents = (
+    vNode: VirtualNode,
+    newVNode: VirtualNode,
+) => {
     removeEvents(vNode.el, vNode.on);
-    addEvents(vNode.el, newVNode.on)
-}
+    addEvents(vNode.el, newVNode.on);
+};
 
-export const attachEvent = (on: {[key: string]: EventListener}, propName: string, handler: EventListener, args: any[]) => {
+export const attachEvent = (
+    on: { [key: string]: EventListener },
+    propName: string,
+    handler: EventListener,
+    args: any[],
+) => {
     const event = propName.slice(prefix.length, propName.length);
+    const value = args.length > 0 ? handler.bind(null, ...args) : handler;
 
     Object.defineProperty(on, event, {
-        value: args.length > 0 ? handler.bind(null, ...args) : handler,
+        value,
         writable: true,
         enumerable: true,
         configurable: true,
     });
 
     return on;
-}
+};
 
